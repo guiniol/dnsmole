16a062baeca953768f4d08275a0920173427b34b
Guillaume Brogi <gui-gui@netcourrier.com>
2016-07-21 22:00:01 +0200

    WIP: Handle subcommands and arguments
    There may be a bug in clap's handling of positional arguments of
    subcommands, so it doesn't work for now.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+#[macro_use]
+extern crate clap;
+
+use clap::{Arg, App, SubCommand};
+
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new(env!("CARGO_PKG_NAME"))
+        .version(crate_version!())
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .subcommand(SubCommand::with_name("serve")
+                    .about("Create a server to relay remote connections")
+                    .arg(Arg::with_name("socket")
+                         .help("port and/or interface to listen on")
+                         .short("p")
+                         .long("port")
+                         .takes_value(true)
+                         )
+                    .arg(Arg::with_name("target")
+                         .help("[ip|hostname:]port to redirect to")
+                         .index(1)
+                         .required(true)
+                         )
+                    )
+        .subcommand(SubCommand::with_name("connect")
+                    .about("Connect to a dnsmole server")
+                    .arg(Arg::with_name("socket")
+                         .help("port and/or interface to listen on")
+                         .short("p")
+                         .long("port")
+                         .takes_value(true)
+                         )
+                    .arg(Arg::with_name("target")
+                         .help("hostname[:port] of the dnsmole server")
+                         .index(1)
+                         .required(true)
+                         )
+                    )
+        .get_matches();
+
+    match matches.subcommand_name() {
+        Some("serve")   => println!("SERVE - target: {}, listen on {}", matches.value_of("target").unwrap(), matches.value_of("socket").unwrap_or("*:25")),
+        Some("connect") => println!("CONNECT - target: {}, listen on {}", matches.value_of("target").unwrap(), matches.value_of("socket").unwrap_or("127.0.0.1:2525")),
+        None            => println!("You must specify an action"),
+        _               => println!("Unknown action"),
+    }
 }
+
