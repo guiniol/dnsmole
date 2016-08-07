fecdbd042608215575d754f7b65a9bc657963c46
Guillaume Brogi <gui-gui@netcourrier.com>
2016-08-07 21:04:30 +0200

    Switch to arggroups instead of subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate clap;
 
-use clap::{Arg, App, SubCommand};
+use clap::{App, Arg, ArgGroup};
 
 
 fn main() {
@@ -9,41 +9,46 @@ fn main() {
         .version(crate_version!())
         .author(env!("CARGO_PKG_AUTHORS"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
-        .subcommand(SubCommand::with_name("serve")
-                    .about("Create a server to relay remote connections")
-                    .arg(Arg::with_name("port")
-                         .help("port and/or interface to listen on (default 53)")
-                         .short("p")
-                         .long("port")
-                         .takes_value(true)
-                        )
-                    .arg(Arg::with_name("target")
-                         .help("[ip|hostname:]port to redirect to")
-                         .index(1)
-                         .required(true)
-                         )
-                    )
-        .subcommand(SubCommand::with_name("connect")
-                    .about("Connect to a dnsmole server")
-                    .arg(Arg::with_name("port")
-                         .help("port and/or interface to listen on (default 5353)")
-                         .short("p")
-                         .long("port")
-                         .takes_value(true)
-                        )
-                    .arg(Arg::with_name("relay")
-                         .help("hostname[:port] of the dnsmole relay")
-                         .index(1)
-                         .required(true)
-                         )
-                    )
+        .arg(Arg::with_name("serve")
+             .help("Create a server to relay remote connections")
+             .short("s")
+             .long("serve")
+             )
+        .arg(Arg::with_name("connect")
+             .help("Connect to a dnsmole server")
+             .short("c")
+             .long("connect")
+             )
+        .group(ArgGroup::with_name("action")
+               .required(true)
+               .args(&["serve", "connect"])
+               )
+        .arg(Arg::with_name("port")
+             .help("port and/or interface to listen on (default 53)")
+             .short("p")
+             .long("port")
+             .takes_value(true)
+             .requires("serve")
+            )
+        .arg(Arg::with_name("target")
+             .help("[ip|hostname:]port to redirect to or dnsmole server to connect to")
+             .index(1)
+             .required(true)
+            )
+        .arg(Arg::with_name("socket")
+             .help("port and/or interface to listen on (default 5353)")
+             .short("k")
+             .long("socket")
+             .takes_value(true)
+             .requires("connect")
+            )
         .get_matches();
 
-    match matches.subcommand_name() {
-        Some("serve")   => println!("SERVE - target: {}, listen on {}", matches.value_of("target").unwrap(), matches.value_of("port").unwrap_or("*:53")),
-        Some("connect") => println!("CONNECT - relay: {}, listen on {}", matches.value_of("relay").unwrap(), matches.value_of("port").unwrap_or("127.0.0.1:5353")),
-        None            => println!("You must specify an action"),
-        _               => println!("Unknown action"),
+    if matches.is_present("serve") {
+        println!("SERVE - target: {}, listen on {}", matches.value_of("target").unwrap(), matches.value_of("port").unwrap_or("*:53"));
+    }
+    if matches.is_present("connect") {
+        println!("CONNECT - relay: {}, listen on {}", matches.value_of("target").unwrap(), matches.value_of("socket").unwrap_or("127.0.0.1:5353"));
     }
 }
 
